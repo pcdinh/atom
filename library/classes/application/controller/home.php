4c56db66541ff133e88cb0a243ccdd04c5450684
Chase Hutchins <syntaqx@gmail.com>
2011-09-11 04:45:17 -0600

    Home controller Added a few comments, and removed the testing action.

--- library/classes/application/controller/home.php
+++ library/classes/application/controller/home.php
@@ -12,24 +12,21 @@ namespace Application\Controller;
 /**
  * Home controller
  *
+ * A basic example of a controller.
+ *
  * @package    Application
  * @subpackage Library
  */
 class Home extends \Controller {
 
 	/**
-	 * 
+	 * Index action
+	 *
+	 * @return   void             No value is returned
 	 */
 	public function action_index()
 	{
 		$this->response->body = \View::make('template')->partial('content', 'home/index');
-	}
-
-	/**
-	 * 
-	 */
-	public function action_test()
-	{
 	}
 }
 
